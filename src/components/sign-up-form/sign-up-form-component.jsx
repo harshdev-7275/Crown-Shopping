6cdd4ad180bb0d0025f985f7599f5702222f0828
harshdev-7275 <111612233+harshdev-7275@users.noreply.github.com>
2023-01-24 02:59:03 +0530

    created authentication page component along side sign in form

--- src/components/sign-up-form/sign-up-form-component.jsx
+++ src/components/sign-up-form/sign-up-form-component.jsx
@@ -6,6 +6,7 @@ import Button from '../button/button.component';
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
+  
 } from '../../utils/firebase/firebase.utils.js';
 
 import './sign-up-form.styles.scss';
